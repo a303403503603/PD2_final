71397b7cd7377d2ccf38402a4d074ef3e743abb3
F74121288 <146808227+F74121288@users.noreply.github.com>
2024-06-11 22:29:59 +0800

    Update GameScreen.java

--- src/userinterface/GameScreen.java
+++ src/userinterface/GameScreen.java
@@ -1,13 +1,16 @@
-package userinterface; // 套件聲明
+package userinterface;
 
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
+import gameobject.BackGround;
 import gameobject.Clouds;
 import gameobject.EnemiesManager;
 import gameobject.Land;
@@ -16,150 +19,174 @@ import util.Resource;
 
 public class GameScreen extends JPanel implements Runnable, KeyListener {
 
-    // 遊戲狀態常數
-    private static final int START_GAME_STATE = 0;
-    private static final int GAME_PLAYING_STATE = 1;
-    private static final int GAME_OVER_STATE = 2;
-    
-    // 遊戲物件
-    private Land land;
-    private MainCharacter mainCharacter;
-    private EnemiesManager enemiesManager;
-    private Clouds clouds;
-    private Thread thread;
+	private static final int START_GAME_STATE = 0;
+	private static final int GAME_PLAYING_STATE = 1;
+	private static final int GAME_OVER_STATE = 2;
+	
+	private Land land;
+	private MainCharacter mainCharacter;
+	private EnemiesManager enemiesManager;
+	private Clouds clouds;
+	private Thread thread;
+	private BackGround backGround; //add
 
-    private boolean isKeyPressed;
+	private boolean isKeyPressed;
 
-    private int gameState = START_GAME_STATE;
+	private int gameState = START_GAME_STATE;
 
-    private BufferedImage replayButtonImage;
-    private BufferedImage gameOverButtonImage;
+	private BufferedImage replayButtonImage;
+	private BufferedImage gameOverButtonImage;
 
-    // 建構函式
-    public GameScreen() {
-        // 初始化遊戲物件
-        mainCharacter = new MainCharacter();
-        land = new Land(GameWindow.SCREEN_WIDTH, mainCharacter);
-        mainCharacter.setSpeedX(4);
-        replayButtonImage = Resource.getResouceImage("data/replay_button.png");
-        gameOverButtonImage = Resource.getResouceImage("data/gameover_text.png");
-        enemiesManager = new EnemiesManager(mainCharacter);
-        clouds = new Clouds(GameWindow.SCREEN_WIDTH, mainCharacter);
-    }
+	private float alpha;    //add
+	private float deltaAlpha;    //add
 
-    // 開始遊戲
-    public void startGame() {
-        thread = new Thread(this);
-        thread.start();
-    }
+	public GameScreen() {
+		mainCharacter = new MainCharacter();
+		land = new Land(GameWindow.SCREEN_WIDTH, mainCharacter);
+		mainCharacter.setSpeedX(4);
+		replayButtonImage = Resource.getResouceImage("C:/Users/User/Downloads/TRexGame/Downasaur/data/replay_button.png");
+		gameOverButtonImage = Resource.getResouceImage("C:/Users/User/Downloads/TRexGame/Downasaur/data/gameover_text.png");
+		enemiesManager = new EnemiesManager(mainCharacter);
+		clouds = new Clouds(GameWindow.SCREEN_WIDTH, mainCharacter);
+		backGround = new BackGround(50); //add
 
-    // 遊戲更新
-    public void gameUpdate() {
-        if (gameState == GAME_PLAYING_STATE) {
-            clouds.update();
-            land.update();
-            mainCharacter.update();
-            enemiesManager.update();
-            if (enemiesManager.isCollision()) {
-                mainCharacter.playDeadSound();
-                gameState = GAME_OVER_STATE;
-                mainCharacter.dead(true);
-            }
-        }
-    }
+	}
 
-    // 繪製遊戲畫面
-    public void paint(Graphics g) {
-        g.setColor(Color.decode("#f7f7f7"));
-        g.fillRect(0, 0, getWidth(), getHeight());
+	public void startGame() {
+		thread = new Thread(this);
+		thread.start();
+	}
 
-        switch (gameState) {
-        case START_GAME_STATE:
-            mainCharacter.draw(g);
-            break;
-        case GAME_PLAYING_STATE:
-        case GAME_OVER_STATE:
-            clouds.draw(g);
-            land.draw(g);
-            enemiesManager.draw(g);
-            mainCharacter.draw(g);
-            g.setColor(Color.BLACK);
-            g.drawString("HI " + mainCharacter.score, 500, 20);
-            if (gameState == GAME_OVER_STATE) {
-                g.drawImage(gameOverButtonImage, 200, 30, null);
-                g.drawImage(replayButtonImage, 283, 50, null);
-            }
-            break;
-        }
-    }
+	public void gameUpdate() {
+		if (gameState == GAME_PLAYING_STATE) {
+			clouds.update();
+			land.update();
+			mainCharacter.update();
+			enemiesManager.update();
+			if (enemiesManager.isCollision()) {
+				mainCharacter.playDeadSound();
+				gameState = GAME_OVER_STATE;
+				mainCharacter.dead(true);
+			}
+		}
+	}
 
-    // 遊戲執行緒
-    @Override
-    public void run() {
-        // 遊戲主迴圈
-        while (true) {
-            gameUpdate();
-            repaint();
-            // 暫停一段時間，控制遊戲更新頻率
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+	
 
-    // 處理按鍵按下事件
-    @Override
-    public void keyPressed(KeyEvent e) {
-        if (!isKeyPressed) {
-            isKeyPressed = true;
-            switch (gameState) {
-            case START_GAME_STATE:
-                if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-                    gameState = GAME_PLAYING_STATE;
-                }
-                break;
-            case GAME_PLAYING_STATE:
-                if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-                    mainCharacter.jump();
-                } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-                    mainCharacter.down(true);
-                }
-                break;
-            case GAME_OVER_STATE:
-                if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-                    gameState = GAME_PLAYING_STATE;
-                    resetGame();
-                }
-                break;
+	public void paint(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.decode("#f7f7f7"));
+		g.fillRect(0, 0, getWidth(), getHeight());
 
-            }
-        }
-    }
+		switch (gameState) {
+		case START_GAME_STATE:
+			mainCharacter.draw(g);
+			break;
+		case GAME_PLAYING_STATE:			
+		case GAME_OVER_STATE:
+			clouds.draw(g);
+			land.draw(g);
+			enemiesManager.draw(g);
+			mainCharacter.draw(g);			
+			g.setColor(Color.BLACK);
+			g.drawString("HI " + mainCharacter.score, 500, 20);
+			if (gameState == GAME_OVER_STATE) {
+				g.drawImage(gameOverButtonImage, 200, 30, null);
+				g.drawImage(replayButtonImage, 283, 50, null);
+				
+			}
+			break;
+		}
+		if(gameState == 1) {
+			backGround.draw(g);     //add
+		}
+		
+		
+		
+	}
 
-    // 處理按鍵釋放事件
-    @Override
-    public void keyReleased(KeyEvent e) {
-        isKeyPressed = false;
-        if (gameState == GAME_PLAYING_STATE) {
-            if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-                mainCharacter.down(false);
-            }
-        }
-    }
+	@Override
+	public void run() {
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-        // TODO Auto-generated method stub
+		int fps = 100;
+		long msPerFrame = 1000 * 1000000 / fps;
+		long lastTime = 0;
+		long elapsed;
+		
+		int msSleep;
+		int nanoSleep;
 
-    }
+		long endProcessGame;
+		long lag = 0;
 
-    // 重置遊戲
-    private void resetGame() {
-        enemiesManager.reset();
-        mainCharacter.dead(false);
-        mainCharacter.reset();
-    }
+		while (true) {
+			gameUpdate();
+			repaint();
+			endProcessGame = System.nanoTime();
+			elapsed = (lastTime + msPerFrame - System.nanoTime());
+			msSleep = (int) (elapsed / 1000000);
+			nanoSleep = (int) (elapsed % 1000000);
+			if (msSleep <= 0) {
+				lastTime = System.nanoTime();
+				continue;
+			}
+			try {
+				Thread.sleep(msSleep, nanoSleep);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			lastTime = System.nanoTime();
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (!isKeyPressed) {
+			isKeyPressed = true;
+			switch (gameState) {
+			case START_GAME_STATE:
+				if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+					gameState = GAME_PLAYING_STATE;
+				}
+				break;
+			case GAME_PLAYING_STATE:
+				if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+					mainCharacter.jump();
+				} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+					mainCharacter.down(true);
+				}
+				break;
+			case GAME_OVER_STATE:
+				if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+					gameState = GAME_PLAYING_STATE;
+					resetGame();
+				}
+				break;
+
+			}
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		isKeyPressed = false;
+		if (gameState == GAME_PLAYING_STATE) {
+			if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+				mainCharacter.down(false);
+			}
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void resetGame() {
+		enemiesManager.reset();
+		mainCharacter.dead(false);
+		mainCharacter.reset();
+	}
 
 }
