84dca8321557750ab21bc29e89bd5ccbcce226dc
F74121288 <146808227+F74121288@users.noreply.github.com>
2024-06-18 03:50:39 +0800

    Update Land.java

--- src/gameobject/BackgroundFile/Land.java
+++ src/gameobject/BackgroundFile/Land.java
@@ -12,26 +12,26 @@ import util.Resource;
 
 public class Land {
     
-    public static final int LAND_POSY = 103; // 地面的Y坐标
+    public static final int LAND_POSY = 103; // 地面的Y座標
     
-    private List<ImageLand> listLand; // 地面图片列表
-    private BufferedImage land1; // 地面图像1
-    private BufferedImage land2; // 地面图像2
-    private BufferedImage land3; // 地面图像3
+    private List<ImageLand> listLand; // 地面圖片列表
+    private BufferedImage land1; // 地面圖像1
+    private BufferedImage land2; // 地面圖像2
+    private BufferedImage land3; // 地面圖像3
     
-    private MainCharacter mainCharacter; // 主角对象
+    private MainCharacter mainCharacter; // 主角對象
     
-    // 构造函数
+    // 建構函數
     public Land(int width, MainCharacter mainCharacter) {
-        this.mainCharacter = mainCharacter; // 设置主角对象
-        // 获取地面图像资源
+        this.mainCharacter = mainCharacter; // 設置主角對象
+        // 獲取地面圖像
         land1 = Resource.getResouceImage("data/land1.png");
         land2 = Resource.getResouceImage("data/land2.png");
         land3 = Resource.getResouceImage("data/land3.png");
-        // 计算需要多少个地面图像来填充屏幕
+        // 計算需要多少個地面圖像填充屏幕
         int numberOfImageLand = width / land1.getWidth() + 2;
-        listLand = new ArrayList<ImageLand>(); // 初始化地面图片列表
-        // 创建地面图片对象并添加到列表中
+        listLand = new ArrayList<ImageLand>(); // 初始化地面圖片列表
+        // 創建地面圖片對象並添加到列表中
         for(int i = 0; i < numberOfImageLand; i++) {
             ImageLand imageLand = new ImageLand();
             imageLand.posX = i * land1.getWidth();
@@ -40,7 +40,7 @@ public class Land {
         }
     }
     
-    // 更新地面状态
+    // 更新land狀態
     public void update(){
         Iterator<ImageLand> itr = listLand.iterator();
         ImageLand firstElement = itr.next();
@@ -59,7 +59,7 @@ public class Land {
         }
     }
     
-    // 设置地面图像
+    // 設置land圖像
     private void setImageLand(ImageLand imgLand) {
         int typeLand = getTypeOfLand();
         if(typeLand == 1) {
@@ -71,14 +71,14 @@ public class Land {
         }
     }
     
-    // 绘制地面
+    // 繪製地面
     public void draw(Graphics g) {
         for(ImageLand imgLand : listLand) {
             g.drawImage(imgLand.image, (int) imgLand.posX, LAND_POSY, null);
         }
     }
     
-    // 获取地面类型
+    // 獲取land類型
     private int getTypeOfLand() {
         Random rand = new Random();
         int type = rand.nextInt(10);
@@ -91,10 +91,10 @@ public class Land {
         }
     }
     
-    // 地面图片的内部类
+    // 地面圖片的內部class
     private class ImageLand {
-        float posX; // X坐标
-        BufferedImage image; // 图像
+        float posX; // X座標
+        BufferedImage image; // 圖像
     }
     
 }
